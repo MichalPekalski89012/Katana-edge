ebb0c674bf1fcfb60c2f371fa6b7530115dd28bc
MichalPekalski89012 <michalpekalski890@gmail.com>
2024-03-07 17:10:43 +0100

    hurtbox gracza w zaleznosci od stanu + naprawa coyote jump

--- Player/player.gd
+++ Player/player.gd
@@ -15,6 +15,7 @@ extends CharacterBody2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var jump_slide_timer = $JumpSlideTimer
 @onready var animation_player = $AnimationPlayer
+@onready var hurtbox = $Hurtbox
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 0
@@ -26,13 +27,13 @@ enum {
 	RUN,
 	CLIMB,
 	SLIDE,
+	JUMP,
 	JUMPSLIDE
 }
 
 var move_state = RUN
 
 func _physics_process(delta):
-#	print("Velocity.x:",velocity.x)
 	attack()
 	apply_gravity(delta)
 	direction = Input.get_axis("left", "right")
@@ -41,11 +42,22 @@ func _physics_process(delta):
 		RUN: run_state(direction,delta)
 		CLIMB: climb_state()
 		SLIDE: slide_state(delta)
+		JUMP: jump_state(delta)
 		JUMPSLIDE: jump_slide_state(direction,delta)
 
 func run_state(direction,delta):
-	jumping()
-	air_resistance(delta)
+	hurtbox.collision_layer = 2048
+	hurtbox.collision_mask = 16384
+
+	if just_left_edge:
+		coyote_jump_timer.start()
+
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+			velocity.y = jump_velocity
+			velocity.x = velocity.x
+			move_state = JUMP
+
 	if Input.is_action_pressed("down") and is_on_floor() and not just_slided:
 		jump_slide_timer.start()
 		move_state = SLIDE
@@ -70,6 +82,8 @@ func climb_state():
 	pass
 
 func slide_state(delta):
+	hurtbox.collision_layer = 1024
+	hurtbox.collision_mask = 8192
 	print("sliduje!!!")
 	velocity.x = move_toward(velocity.x, 0, slide_friction * delta)
 
@@ -85,6 +99,8 @@ func slide_state(delta):
 	move_and_slide()
 
 func jump_slide_state(direction,delta):
+	hurtbox.collision_layer = 4096
+	hurtbox.collision_mask = 32768
 	print("JUMP sliduje!!!")
 	if not is_on_floor():
 		apply_gravity(delta)
@@ -99,12 +115,13 @@ func air_resistance(delta):
 	if not is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, air_resistance_value * delta)
 
-func jumping():
-	if just_left_edge:
-		coyote_jump_timer.start()
-	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = jump_velocity
+func jump_state(delta):
+	hurtbox.collision_layer = 4096
+	hurtbox.collision_mask = 32768
+	if is_on_floor():
+		move_state = RUN
+
+	move_and_slide()
 
 func apply_gravity(delta):
 	if not is_on_floor():
